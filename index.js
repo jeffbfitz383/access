65a6c8969ef5b8d8edb706301d1f5309fc86b4d4
Jeffrey <jefbfitzp33@outlook.com>
2024-08-05 02:03:54 -0600

    able to console log

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
+const { JSDOM } = require("jsdom");
+const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+const document = dom.window.document;
 
+const numOne = 1;
+const numTwo = 2;
+const numTwoStr = numTwo.toString();
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = numTwoStr;
 document.querySelector("body").appendChild(h2);
+console.log(numTwo);
