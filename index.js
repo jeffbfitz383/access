bf9baff0eddd8477f1c4ea4258852eaab24b6aa8
Jeffrey <jefbfitzp33@outlook.com>
2024-08-05 00:01:10 -0600

    inner html works

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("hello world")
-alert("hello");
+
+
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
